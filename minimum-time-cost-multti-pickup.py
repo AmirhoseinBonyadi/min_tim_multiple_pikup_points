0d688d40ce8af539d59fdc6238e5a30fa4c835fa
Amirhosein <amirhoseinbonyadi76@gmail.com>
2023-07-16 16:37:41 +0330

    end of function by on layer

--- minimum-time-cost-multti-pickup.py
+++ minimum-time-cost-multti-pickup.py
@@ -1,30 +1,32 @@
 import numpy as np
-durations = [[0,2,3,4,5,11,12],[6,0,8,9,10,12,33],[11,12,0,14,15,43,21],[2,10,13,0,20,32,12],[11,21,23,24,0,12,11],[1,2,3,4,5,6,7],[1,22,33,44,55,66,43]]
+durations = [[0,2,31,4,51,9,12],[6,0,8,9,10,12,33],[11,12,0,14,15,43,21],[2,10,13,0,20,32,12],[11,21,23,24,0,12,11],[1,2,3,4,5,6,7],[1,22,33,44,55,66,43]]
 driver_count = 2
 trips_count_pickup_list = [2,3]
 
 def optimizer_driver_to_pickup_points(duration_matrix, driver_count, trips_count_pickup_list):
     trips_count = len(trips_count_pickup_list)
     print(duration_matrix)
-
-
-
-
-
-
-
+    INF = 1e10
     horizontal_index = 0
-
+    driver_assigned_to_trips = []
     for i in range(trips_count):
         if(i==0):
             horizontal_index += driver_count
         else: 
             horizontal_index += trips_count_pickup_list[i]-1
-        
-        for j in range(trips_count_pickup_list[i]):
 
+        in_trips_driver_selected=[]
+
+        for j in range(trips_count_pickup_list[i]):
             for k in range(driver_count):
-                print(duration_matrix[k][horizontal_index+j])
+                time = duration_matrix[k][horizontal_index+j]
+                if not(k) and not(j) :
+                    in_trips_driver_selected = [i, j, k, time]
+                else:
+                    if time < in_trips_driver_selected[3]:
+                        in_trips_driver_selected = [i, j, k, time]
+        driver_assigned_to_trips.append(in_trips_driver_selected)
+    print(driver_assigned_to_trips)
             
 
     
