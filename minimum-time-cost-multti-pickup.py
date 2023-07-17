287c5b940871d635588217d103420db5e451aa37
Amirhosein <amirhoseinbonyadi76@gmail.com>
2023-07-17 11:09:01 +0330

    works good!

--- minimum-time-cost-multti-pickup.py
+++ minimum-time-cost-multti-pickup.py
@@ -1,12 +1,41 @@
 import numpy as np
-durations = [[0,2,31,4,51,9,12],[6,0,8,9,10,12,33],[11,12,0,14,15,43,21],[2,10,13,0,20,32,12],[11,21,23,24,0,12,11],[1,2,3,4,5,6,7],[1,22,33,44,55,66,43]]
-driver_count = 2
-trips_count_pickup_list = [2,3]
 
-def optimizer_driver_to_pickup_points(duration_matrix, driver_count, trips_count_pickup_list):
+durations = [[0,2,31,4,51,9,12,88,99],[0,2,31,4,51,9,12,88,7],[6,0,8,7,9,10,12,33,88],[11,77,12,0,14,15,43,21,88],[2,55,10,13,0,20,32,12,88],[11,21,23,24,0,12,11,55,66],[1,2,3,55,4,5,6,7,66],[1,22,33,44,55,66,55,43,55],[1,22,33,44,55,55,66,43,44]]
+drivers_count = 3
+trips_count_pickup_list = [2,3,1]
+
+
+def find_minimum_cost_time_assigning(durations, drivers_count, trips_count_pickup_list):
+    import copy
+    new_durations = copy.deepcopy(durations)
+    new_trips_count_pickup_list = copy.deepcopy(trips_count_pickup_list)
+    all_custumized_assignations = []
+    for step in range(len(trips_count_pickup_list)):
+        step_opt = optimizer_driver_to_pickup_points_in_special_sorting_trips(new_durations, drivers_count, new_trips_count_pickup_list, step)
+        all_custumized_assignations.append(step_opt)
+        change_order_one_step(new_durations, new_trips_count_pickup_list, drivers_count)
+    print('all_custumized_assignations :', all_custumized_assignations)
+    for i in range(len(all_custumized_assignations)):
+        cost_time = 0
+        best_cost_time = 0
+        for j in range(len(trips_count_pickup_list)) : 
+            cost_time += all_custumized_assignations[i][j][3] 
+        print(cost_time) 
+        if not(i):
+            best_assination = all_custumized_assignations[i]
+            best_cost_time = cost_time
+        else :
+            if cost_time < best_cost_time :
+                best_assination = all_custumized_assignations[i]
+                best_cost_time = cost_time
+    
+    return best_assination
+
+def optimizer_driver_to_pickup_points_in_special_sorting_trips(durations, driver_count, trips_count_pickup_list, step):
+    import copy
     trips_count = len(trips_count_pickup_list)
-    print(duration_matrix)
     INF = 1e10
+    duration_matrix = copy.deepcopy(durations)
     horizontal_index = 0
     driver_assigned_to_trips = []
     for i in range(trips_count):
@@ -23,17 +52,23 @@ def optimizer_driver_to_pickup_points(duration_matrix, driver_count, trips_count
                     in_trips_driver_selected = [i, j, k, time]
                 else:
                     if time < in_trips_driver_selected[3]:
-                        in_trips_driver_selected = [i, j, k, time]
+                        trip_index = i+step if i+step < len(trips_count_pickup_list) else i+step-len(trips_count_pickup_list)
+
+                        in_trips_driver_selected = [trip_index, j, k, time]
             selected_driver = in_trips_driver_selected[2]
-            print(selected_driver)
-            duration_matrix[in_trips_driver_selected[2]][0:len(duration_matrix[0])-1]=INF
-            print(duration_matrix)
+        for t in range(len(duration_matrix[in_trips_driver_selected[2]])):
+            duration_matrix[in_trips_driver_selected[2]][t] = INF
         driver_assigned_to_trips.append(in_trips_driver_selected)
     return(driver_assigned_to_trips)
             
-def rotate_trips(durations, driver_count, step):
-    pass
-    
+def change_order_one_step(durations, trips_count_pickup_list, drivers_count):
+    for j in range(drivers_count):
+        for i in range(trips_count_pickup_list[0]):
+            x= durations[j].pop(drivers_count)
+            durations[j].append(x)
+    y = trips_count_pickup_list.pop(0)
+    y = trips_count_pickup_list.append(y)
+    return(durations)
 
 
 
@@ -45,6 +80,10 @@ def rotate_trips(durations, driver_count, step):
 
 
 
+print('minimums',find_minimum_cost_time_assigning(durations, drivers_count, trips_count_pickup_list))
+# print(change_order_one_step(durations, trips_count_pickup_list, driver_count))
+# print(optimizer_driver_to_pickup_points_in_special_sorting_trips(durations, driver_count, trips_count_pickup_list))
 
-optimizer_driver_to_pickup_points(durations, driver_count, trips_count_pickup_list)
-
+# def optimize_driver_to_pickup_points(drivers,trips):
+#     count_trips = len(trips)
+#     count_drivers = len(drivers)
